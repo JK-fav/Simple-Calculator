26c0f9a127d3b179f22ca1aaccc1bcfec5d2aa28
JK-fav <juhi.kothiyal21@gmail.com>
2025-04-14 20:41:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,16 +16,11 @@ function calculateResult() {
   }
 }
 
-// Safe evaluate function
 function safeEvaluate(expr) {
   // Allow only numbers, operators and parentheses
   if (!/^[0-9+\-*/().\s]+$/.test(expr)) {
     throw new Error("Invalid characters in expression");
   }
-
-  // Use Function constructor for safer evaluation
-  // This only evaluates expressions (not arbitrary code)
-  // and avoids access to the wider scope
   const func = new Function(`return (${expr})`);
   return func();
 }
